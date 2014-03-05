28e34fcfca1b68edd26886278f8d737d22eac5c5
Filipe Silva <filipe.av.silva@gmail.com>
2014-03-06 00:35:06 +0100

    Add base folder to allow linking to only the py script. Add new tag

--- addTags.py
+++ addTags.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
 import sys
 import subprocess
+import os
 
 #To be used as a Launchbar action
 #Usage: Pass as command line args unique abbreviations of the tags to add, separated by spaces
 #       e.g. 'ca' or 'cal' for :calm: and 'co' or 'cool' for :cool:, but 'c' won't be recognized as neither.
 #       pass 'reset' to reset the tags
 
+SCRIPT_FOLDER = os.path.dirname(os.path.realpath(__file__))
 SCRIPT_PROGRAM = 'osascript'
 RESET_SCRIPT = 'resetTags.scpt'
 ADD_TAG_SCRIPT = 'AddTag.scpt'
@@ -26,6 +28,7 @@ TAGS = [
     'dance',
     'beat',
     'instrument',
+    'lol',
     'not'
 ]
 
@@ -33,7 +36,7 @@ def main(args):
     tags_to_add = []
 
     if 'reset' in args:
-        subprocess.call([SCRIPT_PROGRAM, RESET_SCRIPT])
+        subprocess.call([SCRIPT_PROGRAM, os.path.join(SCRIPT_FOLDER, RESET_SCRIPT)])
     else:
         for prefix in args:
             matches = [word for word in TAGS if word.startswith(prefix)]
@@ -43,7 +46,7 @@ def main(args):
 
         if tags_to_add != []:
             for tag in tags_to_add: #TODO: change the script so it accepts multiple tags at once
-                subprocess.call([SCRIPT_PROGRAM, ADD_TAG_SCRIPT, tag])
+                subprocess.call([SCRIPT_PROGRAM, os.path.join(SCRIPT_FOLDER, ADD_TAG_SCRIPT), tag])
 
 
 if __name__ == '__main__':
