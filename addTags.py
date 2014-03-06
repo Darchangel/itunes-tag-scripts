487e5ceacd7b933298339f4e46aff4040d7dda7f
Filipe Silva <filipe.av.silva@gmail.com>
2014-03-06 01:12:04 +0100

    Move tags to separate json file. Add sample tag file.
    Now you won't have to deal with merging when I feel like changing my
    tags

--- addTags.py
+++ addTags.py
@@ -1,45 +1,30 @@
 #!/usr/bin/python
 import sys
 import subprocess
-import os
+from os import path
+import json
 
 #To be used as a Launchbar action
 #Usage: Pass as command line args unique abbreviations of the tags to add, separated by spaces
 #       e.g. 'ca' or 'cal' for :calm: and 'co' or 'cool' for :cool:, but 'c' won't be recognized as neither.
 #       pass 'reset' to reset the tags
 
-SCRIPT_FOLDER = os.path.dirname(os.path.realpath(__file__))
+SCRIPT_FOLDER = path.dirname(path.realpath(__file__))
 SCRIPT_PROGRAM = 'osascript'
 RESET_SCRIPT = 'resetTags.scpt'
 ADD_TAG_SCRIPT = 'AddTag.scpt'
 TAG_PATTERN = ':{}:'
+TAGS_FILE = 'tags.json'
 
-TAGS = [
-    'cool',
-    'jump',
-    'feel',
-    'rise',
-    'heavy',
-    'calm',
-    'happy',
-    'sad',
-    'evil',
-    'dark',
-    'dance',
-    'beat',
-    'instrument',
-    'lol',
-    'not',
-    'game',
-    'live'
-]
+json_file = open(path.join(SCRIPT_FOLDER, TAGS_FILE))
+TAGS = json.load(json_file)
 
 def main(args):
     split_args = [actual_arg for sublist in [each_arg.split(' ') for each_arg in args] for actual_arg in sublist]
 
     #Launchbar passes args as one string only. We need to split!
     if 'reset' in split_args:
-        subprocess.call([SCRIPT_PROGRAM, os.path.join(SCRIPT_FOLDER, RESET_SCRIPT)])
+        subprocess.call([SCRIPT_PROGRAM, path.join(SCRIPT_FOLDER, RESET_SCRIPT)])
     else:
         tags_to_add = []
         for prefix in split_args:
@@ -51,7 +36,7 @@ def main(args):
 
         if tags_to_add != []:
             for tag in tags_to_add: #TODO: change the script so it accepts multiple tags at once
-                subprocess.call([SCRIPT_PROGRAM, os.path.join(SCRIPT_FOLDER, ADD_TAG_SCRIPT), tag])
+                subprocess.call([SCRIPT_PROGRAM, path.join(SCRIPT_FOLDER, ADD_TAG_SCRIPT), tag])
 
 
 if __name__ == '__main__':
